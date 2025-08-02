455c20e5d5c32a26a02ef66f6b5f2b9e13ccfe4f
retroblaz <153559220+pranavdixiit@users.noreply.github.com>
2025-08-02 17:22:54 +0530

    Fresh deploy

--- server/utils/sm2.js
+++
@@ -1,9 +0,0 @@
-// server/utils/sm2.js
-export const updateSM2 = (card, q) => {
-  let ef = card.easeFactor + (0.1 - (5 - q) * (0.08 + (5 - q) * 0.02));
-  if (ef < 1.3) ef = 1.3;
-  let reps = q < 3 ? 0 : card.repetitions + 1;
-  let intv = reps === 0 ? 1 : reps === 1 ? 1 : reps === 2 ? 6 : Math.round(card.interval * ef);
-  let next = new Date(Date.now() + intv * 24 * 60 * 60 * 1000);
-  return { easeFactor: ef, repetitions: reps, interval: intv, nextReview: next };
-};
